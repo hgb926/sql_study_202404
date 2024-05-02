356087f7a0a57679238c01bad0ecd02f36f01834
hgb926 <hgb926@naver.com>
2024-05-02 12:49:32 +0900

    6. inner join

--- human-resource.sql
+++ human-resource.sql
@@ -1,98 +1,118 @@
--- 1. employees 테이블에서 모든 사원의 first_name을 대문자로 변환하여 출력하세요.
-SELECT
-    UPPER(first_name)
-FROM employees;
-
--- 2. employees 테이블에서 모든 사원의 last_name을 소문자로 변환하여 출력하세요.
-SELECT
-    LOWER(last_name)
-FROM employees;
-
--- 3. employees 테이블에서 각 사원의 hire_date의 연도만 추출하여 출력하세요.
-SELECT
-    SUBSTR(hire_date, 1, 4)
-FROM employees;
-
--- 4. employees 테이블에서 각 사원의 hire_date의 월과 일을 연결하여 출력하세요. 예: MM-DD 형식.
-SELECT
-     SUBSTR(hire_date, 6, 2) ||'-' ||SUBSTR(hire_date, 9, 2)
-FROM employees;
+-- 실습문제
 
 
--- 5. departments 테이블에서 모든 부서의 department_name의 앞 두 글자만 출력하세요.
-SELECT
-    SUBSTR(department_name, 1, 2)
-FROM departments;
+-- 1. employees테이블에서 각 사원의 부서별 부서 번호(department_id)와 
+--    평균 급여(salary)를 조회하세요. 
+--    단, 부서번호가 null이면 0으로 변경하여 조회세요.
 
--- 6. employees 테이블에서 manager_id가 NULL인 사원의 이름과 직책을 출력하세요.
 SELECT
-    first_name || last_name,
+    NVL(department_id, 0),
+    ROUND(avg(salary))
+FROM employees
+GROUP BY department_id
+;
+
+-- 2. employees테이블에서 부서별 부서 번호(department_id)와 부서별 총 사원 수를 조회하세요.
+--    단, 부서번호가 null이면 0으로 변경하여 조회세요.
+
+SELECT
+    NVL(department_id, 0),
+    COUNT(*)
+FROM employees
+GROUP BY department_id
+;
+
+
+
+-- 3. employees테이블에서 부서의 급여 평균이 8000을 초과하는 부서의 부서번호와 급여 평균을 조회하세요.
+SELECT
+    department_id,
+    AVG(salary)
+FROM employees
+WHERE AVG(salary) > 8000
+;
+
+SELECT
+    department_id,
+    ROUND(AVG(salary), 2) AS avg_sal
+FROM employees
+GROUP BY department_id
+HAVING AVG(salary) > 8000
+ORDER BY department_id
+;
+
+
+-- 4. employees테이블에서 급여 평균이 8000을 초과하는 각 직무(job_id)에 대하여 
+-- 직무 이름(job_id)이 SA로 시작하는 사원은 제외하고 직무 이름과 직무별 급여 평균을 
+--    급여 평균이 높은 순서로 정렬하여 조회하세요.
+
+SELECT
+    job_id,
+    AVG(salary)
+FROM employees
+WHERE job_id NOT LIKE 'SA%'
+GROUP BY job_id
+HAVING AVG(salary) > 8000
+ORDER BY AVG(salary) DESC
+;
+SELECT 
     job_id
+    , ROUND(AVG(salary), 2) AS avg_sal
 FROM employees
-WHERE manager_id IS NULL;
+WHERE job_id NOT LIKE 'SA%'
+GROUP BY job_id
+HAVING AVG(salary) > 8000
+ORDER BY avg_sal DESC
+;
 
--- 7. jobs 테이블에서 job_title의 길이를 계산하여 출력하세요.
-SELECT
-    LENGTH(job_title)
-FROM jobs;
+-- 실습문제
 
--- 8. employees 테이블에서 사원의 이름을 성과 이름으로 나누어 각각 '성', '이름' 별칭으로 출력하세요.
-SELECT
-    first_name 성
-    ,last_name 이름
-FROM employees;
 
--- 9. employees 테이블에서 각 사원의 성의 첫 글자와 이름을 연결하여 출력하세요.
-SELECT
-    SUBSTR(first_name, 1, 1) || last_name
-FROM employees;
-
--- 10. locations 테이블에서 city의 이름을 역으로 출력하세요. 
-SELECT
-    REVERSE(city)
-FROM locations;
-
--- 11. employees 테이블에서 각 사원의 salary를 100달러 단위로 올림하여 출력하세요.
-SELECT
-    ROUND(salary, -2)
-    ,salary
-FROM employees;
-
--- 12. employees 테이블에서 각 사원의 commission_pct가 NULL일 경우, 0으로 표시하여 출력하세요.
-SELECT
-    NVL(commission_pct, 0)
-FROM employees;
-
--- 13. employees 테이블에서 각 사원의 이름의 첫 글자와 성의 첫 글자를 연결하여 출력하세요.
-SELECT
-    SUBSTR(last_name,1,1) || SUBSTR(first_name,1,1)
-FROM employees;
-
--- 14. employees 테이블에서 각 사원의 salary에 대해 10% 인상한 금액을 계산하여 출력하세요.
-SELECT
-    salary * 1.1
-FROM employees;
-
--- 15. countries 테이블에서 각 국가의 이름을 세 글자로 줄여서 출력하세요.
-SELECT
-    SUBSTR(country_name, 1, 3)
-FROM countries;
-
--- 16. employees 테이블에서 각 사원의 last_name의 두 번째 글자를 '*'로 변경하여 출력하세요
-SELECT
-    SUBSTR(last_name,1,1) || '*' || SUBSTR(last_name,3)
-FROM employees;
-
--- 17. employees 테이블에서 job_id가 it_prog인 사원의 first_name과 salary를 출력하세요.
---  조건1 ) 비교하기 위한 값은 소문자로 입력할 것!
---  조건2 ) 이름은 앞 3글자까지만 출력하고 나머지는 *로 마킹할 것. 
---          이 열의 별칭은 name입니다.
-
-SELECT
-    RPAD(SUBSTR(first_name,1, 3), LENGTH(first_name), '*') name,
-    salary
+-- 1. employees테이블에서 각 사원의 부서별 부서 번호(department_id)와 
+--    평균 급여(salary)를 조회하세요. 
+--    단, 부서번호가 null이면 0으로 변경하여 조회세요.
+SELECT 
+  NVL(department_id, 0) AS dep_id
+  , ROUND(AVG(salary), 2) AS avg_salary
 FROM employees
-WHERE LOWER(job_id) = 'it_prog';
+GROUP BY department_id
+ORDER BY dep_id
+;
 
 
 
+-- 2. employees테이블에서 부서별 부서 번호(department_id)와 부서별 총 사원 수를 조회하세요.
+--    단, 부서번호가 null이면 0으로 변경하여 조회세요.
+SELECT 
+    NVL(department_id, 0) AS dep_id
+     , COUNT(employee_id) AS total_count
+FROM employees
+GROUP BY department_id
+ORDER BY dep_id
+;
+
+
+-- 3. employees테이블에서 부서의 급여 평균이 8000을 초과하는 부서의 부서번호와 급여 평균을 조회하세요.
+SELECT
+    department_id,
+    ROUND(AVG(salary), 2) AS avg_sal
+FROM employees
+GROUP BY department_id
+HAVING AVG(salary) > 8000
+ORDER BY department_id
+;
+
+
+
+-- 4. employees테이블에서 급여 평균이 8000을 초과하는 각 직무(job_id)에 대하여 
+--    직무 이름(job_id)이 SA로 시작하는 사원은 제외하고 직무 이름과 직무별 급여 평균을 
+--    급여 평균이 높은 순서로 정렬하여 조회하세요.
+SELECT 
+    job_id
+    , ROUND(AVG(salary), 2) AS avg_sal
+FROM employees
+WHERE job_id NOT LIKE 'SA%'
+GROUP BY job_id
+HAVING AVG(salary) > 8000
+ORDER BY avg_sal DESC
+;
